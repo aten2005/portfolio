36d426b79a2080e2cde313c045d01d238d01d773
Atendimento2005 <gogo.june2005@gmail.com>
2024-07-27 23:17:34 +0530

    finished landing page

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -2,13 +2,19 @@ import logo from "public/logo.svg";
 export default function Header() {
   return (
     <div className="w-screen h-20 flex flex-row items-center justify-end max-w-screen-xl">
-      <a href="/" className="mr-auto font-head text-xl ml-7">
-        <img src={logo.src} className="h-5"></img>
+      <a href="/" className="mr-auto font-head ml-5 md:ml-8">
+        <img src={logo.src} className="h-5 md:h-7"></img>
       </a>
-      <a href="/projects" className="text-lg">
+      <a
+        href="/projects"
+        className="text-lg font-sans montserrat hover:text-primary md:text-2xl"
+      >
         Projects
       </a>
-      <a href="/blog" className="text-lg mr-7 ml-7">
+      <a
+        href="/blog"
+        className="text-lg mr-7 ml-7 font-sans montserrat hover:text-primary md:text-2xl md:mr-10 md:ml-10"
+      >
         Blog
       </a>
     </div>
--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,30 +1,59 @@
-import profile from "public/profile.jpg";
+import profile from "/profile.jpg?url";
 import Typewriter from "typewriter-effect";
+import github from "/icons/github.svg?url";
+import linkedin from "/icons/linkedin.svg?url";
+import twitter from "/icons/twitter.svg?url";
+import discord from "/icons/discord.svg?url";
+import instagram from "/icons/instagram.svg?url";
+import mail from "/icons/mail.svg?url";
 export default function Hero() {
   return (
-    <div className="flex flex-col items-center justify-around space-y-7 md:flex-row-reverse mt-20 md:mt-36">
-      <img
-        src={profile.src}
-        alt="profile-picture"
-        className="rounded-full w-64 md:w-72 border-4 border-solid"
-      />
-      <div className="md:mr-8">
-        <p className="text-4xl/[3.25rem] font-medium poppins md:text-6xl/[5rem] ">
-          Hey👋
-          <br />
-          I'm{" "}
-          <span className="underline underline-offset-8 decoration-primary">
-            Archisman Das
-          </span>
-          <br />
-          <Typewriter
-            options={{
-              strings: ["Learner", "Developer", "Engineer"],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-        </p>
+    <div className="h-[calc(100dvh-5rem)]">
+      <div className="flex flex-col items-center justify-around space-y-7 md:flex-row-reverse mt-12 md:mt-36">
+        <img
+          src={profile}
+          alt="profile"
+          className="rounded-full w-64 md:w-[22rem] border-4 border-solid"
+        />
+        <div className="md:mr-8">
+          <p className="text-4xl/[3.25rem] font-medium poppins md:text-6xl/[5rem] ">
+            Hey👋
+            <br />
+            I'm{" "}
+            <span className="underline underline-offset-[0.5rem] decoration-primary decoration-[.2rem]">
+              Archisman Das
+            </span>
+            <br />
+            <Typewriter
+              aria-label="Learner, Developer, Engineer"
+              options={{
+                strings: ["Learner", "Developer", "Engineer"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-row space-x-5 md:space-x-9 justify-center mt-24 ">
+        <a href="https://linkedin.com/in/aten2005">
+          <img src={linkedin} alt="linkedin" className="h-6 md:h-8" />
+        </a>
+        <a href="https://github.com/aten2005">
+          <img src={github} alt="github" className="h-6 md:h-8" />
+        </a>
+        <a href="https://x.com/aten_2005">
+          <img src={twitter} alt="twitter" className="h-6 md:h-8" />
+        </a>
+        <a href="">
+          <img src={discord} alt="discord" className="h-6 md:h-8" />
+        </a>
+        <a href="">
+          <img src={instagram} alt="instagram" className="h-6 md:h-8" />
+        </a>
+        <a href="mailto:archisman0306@gmail.com">
+          <img src={mail} alt="mail" className="h-6 md:h-8" />
+        </a>
       </div>
     </div>
   );
